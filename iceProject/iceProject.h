68ad9507f83ca6fdb0ea9d4ff684fcfd2f622f4c
abeebsuprbiz <isola@suprbiz.io>
2020-11-24 09:14:19 +0100

    icecream shop

---
+++ iceProject/iceProject.h
@@ -0,0 +1,18 @@
+//
+//  iceProject.h
+//  iceProject
+//
+//  Created by MBUSER on 24/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iceProject.
+FOUNDATION_EXPORT double iceProjectVersionNumber;
+
+//! Project version string for iceProject.
+FOUNDATION_EXPORT const unsigned char iceProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iceProject/PublicHeader.h>
+
+
